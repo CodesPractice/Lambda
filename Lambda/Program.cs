322e709e6c4e051327734e3cf8c5bc4a87728383
Dinushika <dprasangani@gmail.com>
2025-07-05 20:43:47 +0530

    Complete Expression lambda

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        delegate int MyDelegate(int x);
+
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // Expression lambda: square of a number
+            MyDelegate square = x => x * x;
+            Console.WriteLine("Square of 5: " + square(5));  // Output: 25
+
+            // Expression lambda: multiply by 10
+            MyDelegate multiplyByTen = x => x * 10;
+            Console.WriteLine("10 times 3: " + multiplyByTen(3));  // Output: 30
+
+            Console.ReadKey();  
+        }
+
+        public static void ExpressionLambda()
+        {
+
         }
     }
 }
