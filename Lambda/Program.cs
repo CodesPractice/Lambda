cbfb88059304454a361b159a5d8ff1692836f2e5
Dinushika <dprasangani@gmail.com>
2025-07-05 21:26:09 +0530

    Re-implement Piggy Bank challange using lambdaa

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -6,31 +6,17 @@
 
         static void Main()
         {
-            // ExpressionLambda();
+            ExpressionLambda();
 
-            // Statement lambda: check even or odd
-            MyDelegate evenOrOdd = x =>
-                {
-                    if (x % 2 == 0)
-                    {
-                        Console.WriteLine($"{x} is Even");
-                        return 1;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{x} is Odd");
-                        return 0;
-                    }
-                };
+            StatementLambda();
 
-            evenOrOdd(7);  // Output: 7 is Odd
-            evenOrOdd(4);  // Output: 4 is Even
 
             Console.ReadKey();
         }
 
         public static void ExpressionLambda()
         {
+            Console.WriteLine("Expression lambda\n-----------------------------------------");
             // Expression lambda: square of a number
             MyDelegate square = x => x * x;
             Console.WriteLine("Square of 5: " + square(5));  // Output: 25
@@ -38,8 +24,30 @@
             // Expression lambda: multiply by 10
             MyDelegate multiplyByTen = x => x * 10;
             Console.WriteLine("10 times 3: " + multiplyByTen(3));  // Output: 30
+
+            Console.WriteLine("\n\n");
         }
 
-       
+        public static void StatementLambda()
+        {
+            Console.WriteLine("Statement lambda\n-----------------------------------------");
+            // Statement lambda: check even or odd
+            MyDelegate evenOrOdd = x =>
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine($"{x} is Even");
+                    return 1;
+                }
+                else
+                {
+                    Console.WriteLine($"{x} is Odd");
+                    return 0;
+                }
+            };
+
+            evenOrOdd(7);  // Output: 7 is Odd
+            evenOrOdd(4);  // Output: 4 is Even
+        }
     }
 }
---
+++ LambdaEvent/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace LambdaEvent
+{
+
+    class BankAccount
+    {
+        public delegate void BalanceChangedHandler(int newBalance);
+        public event BalanceChangedHandler OnBalanceChanged;
+
+        private int balance = 0;
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+
+            // Trigger the event when balance changes
+            OnBalanceChanged?.Invoke(balance);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+
+            // Subscribe to the event using a lambda expression
+            account.OnBalanceChanged += newBalance =>
+            {
+                Console.WriteLine($"Balance updated: {newBalance}");
+            };
+
+
+            // Another subscriber with a lambda to show a message if goal reached
+            account.OnBalanceChanged += newBalance =>
+            {
+                if (newBalance >= 500)
+                    Console.WriteLine("You reached your savings goal!");
+            };
+
+
+            string input;
+            do
+            {
+                Console.Write("How much do you want to deposit? : ");
+                input = Console.ReadLine();
+
+
+                if (int.TryParse(input, out int amount))
+                {
+                    account.Deposit( amount);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+
+            } while (input.ToLower() != "exit");
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
