5fec76e8fc62d97c2bd88217b23e2d0ba1efc495
Dinushika <dprasangani@gmail.com>
2025-07-05 20:46:20 +0530

    Completed statement lambda

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -6,6 +6,31 @@
 
         static void Main()
         {
+            // ExpressionLambda();
+
+            // Statement lambda: check even or odd
+            MyDelegate evenOrOdd = x =>
+                {
+                    if (x % 2 == 0)
+                    {
+                        Console.WriteLine($"{x} is Even");
+                        return 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{x} is Odd");
+                        return 0;
+                    }
+                };
+
+            evenOrOdd(7);  // Output: 7 is Odd
+            evenOrOdd(4);  // Output: 4 is Even
+
+            Console.ReadKey();
+        }
+
+        public static void ExpressionLambda()
+        {
             // Expression lambda: square of a number
             MyDelegate square = x => x * x;
             Console.WriteLine("Square of 5: " + square(5));  // Output: 25
@@ -13,13 +38,8 @@
             // Expression lambda: multiply by 10
             MyDelegate multiplyByTen = x => x * 10;
             Console.WriteLine("10 times 3: " + multiplyByTen(3));  // Output: 30
-
-            Console.ReadKey();  
         }
 
-        public static void ExpressionLambda()
-        {
-
-        }
+       
     }
 }
